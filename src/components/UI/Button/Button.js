9497a5d4e0743046f7131a12ead05c12278d1675
rextambua <rextambua1@gmail.com>
2023-06-27 23:04:20 +0300

    added other hooks and some refactoring

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,14 +4,14 @@ import classes from './Button.module.css';
 
 const Button = (props) => {
   return (
-    <button
-      type={props.type || 'button'}
-      className={`${classes.button} ${props.className}`}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.children}
-    </button>
+      <button
+          type={props.type || 'button'}
+          className={`${classes.button} ${props.className}`}
+          onClick={props.onClick}
+          disabled={props.disabled}
+      >
+        {props.children}
+      </button>
   );
 };
 
